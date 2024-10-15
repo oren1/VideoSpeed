7232a6dde452a0e891639f16fe0e10f06398a188
Oren Shalev <orenico4@gmail.com>
2024-10-15 16:59:37 +0300

    successful rebase with main

---
+++ VideoSpeed/Edit Sections/CropSectioVC.swift
@@ -0,0 +1,43 @@
+//
+//  CropSectioVC.swift
+//  VideoSpeed
+//
+//  Created by oren shalev on 11/10/2024.
+//
+
+import UIKit
+
+enum CropStatus {
+    case done, cropping
+}
+
+typealias CropSectionChangedStatusTo = (CropStatus) -> Void
+
+class CropSectioVC: SectionViewController {
+   
+    @IBOutlet weak var cropButton: UIButton!
+    var cropStatus: CropStatus = .cropping
+    var cropSectionChangedStatusTo: CropSectionChangedStatusTo?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        cropButton.setTitle("Done", for: .normal)
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func cropButtonTapped(_ sender: Any) {
+        if cropStatus == .cropping {
+            cropStatus = .done
+            cropButton.setTitle("Crop", for: .normal)
+        }
+        else if cropStatus == .done {
+            cropStatus = .cropping
+            cropButton.setTitle("Done", for: .normal)
+        }
+        
+        cropSectionChangedStatusTo?(cropStatus)
+    }
+    
+
+}
